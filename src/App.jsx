9075f2662d4fee6d5770128f1eea7766e403ed60
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-07-27 09:48:28 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-3xl font-semibold text-blue-800">Hello Everyone</p>
+      <p className="text-3xl font-semibold text-blue-700">Hello Everyone</p>
+    </>
+  );
+}
+
+export default App;

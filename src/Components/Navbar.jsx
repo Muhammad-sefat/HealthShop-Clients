2ef99af70ce925d1c8a0866fdeed2df9ffd62bd6
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-07-28 14:36:23 +0600

    Complete Footer section

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -95,7 +95,12 @@ const Navbar = () => {
               <div className="w-10 rounded-full">
                 <img
                   alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  referrerPolicy="no-referrer"
+                  src={
+                    user && user.photoURL
+                      ? user.photoURL
+                      : "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  }
                 />
               </div>
             </div>
